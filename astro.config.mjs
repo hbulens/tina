2d1bd5d75bfcd3752be7b54c6383b556a1e568cf
Hendrik Bulens <hendrik.bulens@gmail.com>
2024-08-19 11:43:20 +0200

    Content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(),react()],
+  integrations: [tailwind(), sitemap(), react()],
   site: "https://hbulens.github.io",
   base: "/the-average-cyclist",
   output: "static",
--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,20 +1,20 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
-    const posts = await getCollection('posts');
+  const posts = await getCollection("posts");
 
-    return rss({
-        title: 'The Average Cyclist blog',
-        description: 'Cycling routes across Spain',
-        site: context.site,
-        items: posts.map((post) => ({
-            title: post.data.title,
-            pubDate: post.data.pubDate,
-            description: post.data.description,
-            customData: post.data.customData,
-            link: `/posts/${post.id}/`,
-        })),
-        customData: `<language>en-us</language>`,
-    });
+  return rss({
+    title: "The Average Cyclist blog",
+    description: "Cycling routes across Spain",
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      customData: post.data.customData,
+      link: `/posts/${post.id}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
 }
