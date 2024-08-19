ec4d49018b5d3ab70bbdca5ca12241315d989e36
Hendrik Bulens <hendrik.bulens@gmail.com>
2024-08-19 14:43:52 +0200

    Build

--- astro.config.ts
+++ astro.config.ts
@@ -3,11 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
-import mdx from "@astrojs/mdx";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), react(), mdx()],
+  integrations: [tailwind(), sitemap(), react()],
   site: "https://hbulens.github.io",
   base: "/the-average-cyclist",
   output: "static"
