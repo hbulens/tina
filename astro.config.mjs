9e58ac7d553f5c28570f6c37f35c9ce9cb34bacc
Hendrik Bulens <hendrik.bulens@gmail.com>
2024-08-19 07:11:19 +0200

    Output

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://hbulens.github.io',
-  base: 'the-average-cyclist',
+    integrations: [tailwind()],
+    site: 'https://hbulens.github.io',
+    base: 'the-average-cyclist',
+    output: 'static'
 });
