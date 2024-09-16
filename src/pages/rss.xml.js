89c5f604a058b09ab9e8223d70de02c41e0b4fb7
Hendrik Bulens <hendrik.bulens@gmail.com>
2024-09-16 07:37:38 +0200

    Route difficulty rating

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,16 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+
+    return rss({
+        title: 'The Average Cyclist',
+        description: 'Routes and inspiration for your next cycling trip',
+        site: context.site,
+        items: await pagesGlobToRssItems(
+            import.meta.glob('./posts/*.{md,mdx}'),
+        ),
+        customData: `<language>en-us</language>`,
+    });
+}
