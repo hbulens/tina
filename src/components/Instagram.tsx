491f9662205bc68bd2c447df4d928c56da3337cb
Hendrik Bulens <hendrik.bulens@gmail.com>
2024-09-16 07:07:58 +0200

    1 IG post

--- src/components/Instagram.tsx
+++ src/components/Instagram.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { InstagramEmbed } from 'react-social-media-embed';
 
 const Instagram = () => {
     useEffect(() => {
@@ -16,53 +17,18 @@ const Instagram = () => {
 
             {/* Instagram Posts */}
             <div className="flex justify-center items-center">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-1 gap-4">
                     {/* First Instagram Post */}
                     <div className="flex justify-center items-center">
                         <div className="flex flex-col justify-center items-center">
-                            <blockquote
-                                className="instagram-media"
-                                data-instgrm-permalink="https://www.instagram.com/p/C_8FiQPNC8O/"
-                                data-instgrm-version="14"
-                                style={{
-                                    background: '#FFF',
-                                    border: '0',
-                                    borderRadius: '3px',
-                                    boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)',
-                                    margin: '1px',
-                                    maxWidth: '540px',
-                                    minWidth: '500px',
-                                    padding: '0',
-                                    width: 'calc(100% - 2px)',
-                                }}
-                            />
-                        </div>
-                    </div>
-
-                    {/* Second Instagram Post */}
-                    <div className="flex justify-center items-center">
-                        <div className="flex flex-col justify-center items-center">
-                            <blockquote
-                                className="instagram-media"
-                                data-instgrm-permalink="https://www.instagram.com/p/C_fTbIOu7bn"
-                                data-instgrm-version="14"
-                                style={{
-                                    background: '#FFF',
-                                    border: '0',
-                                    borderRadius: '3px',
-                                    boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)',
-                                    margin: '1px',
-                                    maxWidth: '540px',
-                                    minWidth: '500px',
-                                    padding: '0',
-                                    width: 'calc(100% - 2px)',
-                                }}
-                            />
+                            <InstagramEmbed url="https://www.instagram.com/p/C_8FiQPNC8O/" width={500} captioned />
                         </div>
                     </div>
                 </div>
                 <script async defer src="//www.instagram.com/embed.js"></script>
             </div>
+
+
         </>
     );
 };
